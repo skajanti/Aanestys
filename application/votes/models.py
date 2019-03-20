1325d3d42980c72e0a4dc0f9b175154be5b41d72
Seppo Kajantie <seppo.kajantie@gmail.com>
2019-03-20 15:14:22 +0200

    tietokanta toimii

--- application/votes/models.py
+++ application/votes/models.py
@@ -11,7 +11,7 @@ class Candidate(db.Model):
 class Vote(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	candidate_id = db.Column(db.Integer, db.ForeignKey("candidate.id"), nullable=False)
-	voter_id = db.Column(db.Integer, db.ForeignKey("voter.id"), nullable.False)
+	voter_id = db.Column(db.Integer, db.ForeignKey("voter.id"), nullable=False)
 	year = db.Column(db.Integer)
 
 	def __init__(self, year):
@@ -19,7 +19,7 @@ class Vote(db.Model):
 
 class Voter(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(32)
+	name = db.Column(db.String(32))
 
 	def __init__(self, name):
-		self.name = name
+		return self.name
