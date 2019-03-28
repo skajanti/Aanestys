37acdf7d5aca34644ea1a8090d152cfa94410d81
Seppo Kajantie <seppo.kajantie@gmail.com>
2019-03-28 19:43:55 +0200

    Revert "postgre herokuun"
    This reverts commit 9c9183ea7688a403a1cf0c56d0c3b6e31d1c7d9d.

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,17 @@
-
+#tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
-
+# Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
+# Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
+# samassa paikassa
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///votes.db"
+# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+app.config["SQLALCHEMY_ECHO"] = True
 
-if os.environ.get("HEROKU"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///votes.db"
-    app.config["SQLALCHEMY_ECHO"] = True
-
-
+# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
 db = SQLAlchemy(app)
 
 from application import views
@@ -40,8 +40,4 @@ def load_user(user_id):
 
 
 # Luodaan lopulta tarvittavat tietokantataulut
-
-try: 
-    db.create_all()
-except:
-    pass
+db.create_all()
