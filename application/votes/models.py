f3db936c5c4777c2107e025ddabb34efdc4f0d48
Seppo Kajantie <seppo.kajantie@gmail.com>
2019-04-04 23:17:08 +0300

    Messed with vote count

--- application/votes/models.py
+++ application/votes/models.py
@@ -14,16 +14,17 @@ class Candidate(db.Model):
 		
 	@staticmethod
 	def count_votes(done=0):
-		stmt = text("SELECT count(Vote.id) FROM Vote LEFT JOIN Candidate ON Vote.candidate_id = Candidate.id GROUP BY Candidate.id").params(done=done)
+		stmt = text("SELECT Candidate.name, count(Vote.id) FROM Vote LEFT JOIN Candidate ON Vote.candidate_id = Candidate.id GROUP BY Candidate.id").params(done=done)
 		
 		res = db.engine.execute(stmt)
 		
 		response = []
 		
 		for row in res:
-			response.append({"count(id)":row[0]})
+			response.append({row[0], row[1]})
 		
 		return response
+
 
 class Vote(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
